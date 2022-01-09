608529f614d73e242eaa3034682794834213cc34
Trider <47952308+Trider@users.noreply.github.com>
2022-01-09 14:07:50 +0600

    Reduced default number of circle points

--- Core/include/Node/Primitive2D.hpp
+++ Core/include/Node/Primitive2D.hpp
@@ -10,7 +10,7 @@ class Primitive2D : public Node2D
 public:
 	Primitive2D();
 	Primitive2D(const sf::Color& color, const sf::Vector2f& position, const sf::Vector2f& dimensions);
-	Primitive2D(const sf::Color& color, const sf::Vector2f& position, float radius, unsigned numberOfPoints = 32);
+	Primitive2D(const sf::Color& color, const sf::Vector2f& position, float radius, unsigned numberOfPoints = 24);
 	~Primitive2D();
 
 	const sf::Color& getColor() const;
