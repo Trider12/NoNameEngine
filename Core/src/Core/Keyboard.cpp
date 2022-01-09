58d88cea6ef50d987439fe7106853f2c109c2d38
Trider <47952308+Trider@users.noreply.github.com>
2022-01-09 12:39:46 +0600

    Simplified keyboard proxy

--- Core/src/Core/Keyboard.cpp
+++ Core/src/Core/Keyboard.cpp
@@ -1,8 +1,7 @@
 #include "Core/Keyboard.hpp"
 
-#include <SFML/Window/Keyboard.hpp>
-
-bool Keyboard::isKeyPressed(Key key)
+#pragma warning(disable : 26812) // FFS Microsoft
+bool Keyboard::isKeyPressed(Keyboard::Key key)
 {
-	return sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(key));
+	return sf::Keyboard::isKeyPressed(key);
 }
